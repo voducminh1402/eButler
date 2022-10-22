f75177e375e31c433d8cd3af12d39e201eb570ab
quangquyen2508 <tranquangquyen2013@gmail.com>
2022-10-23 02:13:05 +0700

    [QuyenTQ] add page Category

---
+++ eButler/Pages/Category.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BusinessLogic.Models;
+
+namespace eButler.Pages
+{
+    public class CategoryModel : PageModel
+    {
+        private readonly BusinessLogic.Models.eButlerContext _context;
+
+        public CategoryModel(BusinessLogic.Models.eButlerContext context)
+        {
+            _context = context;
+        }
+
+        public IList<ProductSupplier> listProductSupplier { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            listProductSupplier = await _context.ProductSuppliers.ToListAsync();
+        }
+    }
+}
