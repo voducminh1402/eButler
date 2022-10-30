1f5fa2db5a7357f154072445b9c1b8bb884a4823
learning2html <honganhle0a@gmail.com>
2022-10-30 22:52:40 +0700

    [AnhLH] fix paypal payment

---
+++ DataAccess/Repostiories/CheckOutRepository.cs
@@ -0,0 +1,27 @@
+﻿using DataAccess.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repostiories
+{
+    public interface ICheckOutRepository
+    {
+        bool GetCheckOutStatus();
+        void SetCheckOutStatus(bool status);
+    }
+    public class CheckOutRepository : ICheckOutRepository
+    {
+        public bool GetCheckOutStatus()
+        {
+            return CheckOutService.GetInstance.GetCheckOutSuccess;
+        }
+
+        public void SetCheckOutStatus(bool status)
+        {
+            CheckOutService.GetInstance.GetCheckOutSuccess = status;
+        }
+    }
+}
